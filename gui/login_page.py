34f432a33ce82710c65fa692a8f71296a804ef1f
mj-003 <272660@student.pwr.edu.pl>
2024-05-28 18:28:50 +0200

    registration added

--- gui/login_page.py
+++ gui/login_page.py
@@ -1,9 +1,7 @@
 from PIL import Image
 from customtkinter import *
-
-from gui import const
+from registration_window import RegistrationWindow
 from user import User
-from const import *
 
 
 class LoginPage(CTkFrame):
@@ -12,81 +10,55 @@ class LoginPage(CTkFrame):
 
         self.app = app
         self.parent = parent
-        self.database = Database('expenses.db')
-        print("______")
-        print(self.database.get_columns())
+        self.database = database
 
         self.user = None
         self.username_entry = None
         self.password_entry = None
 
         self.frame = None
+        self.email_icon = CTkImage(dark_image=Image.open("images/email-icon.png"), size=(20, 20))
+        self.password_icon = CTkImage(dark_image=Image.open("images/password-icon.png"), size=(17, 17))
+
         self.add_image()
         self.add_log_pass()
         self.login()
+        self.add_register()
 
     def add_image(self):
         side_img_data = Image.open("images/side-img.png")
         side_img = CTkImage(dark_image=side_img_data, light_image=side_img_data, size=(400, 745))
 
-        CTkLabel(master=self, text="", image=side_img).pack(
-            expand=True,
-            side="left")
+        CTkLabel(master=self, text="", image=side_img).pack(expand=True, side="left")
 
     def add_log_pass(self):
-        email_icon_data = Image.open("images/email-icon.png")
-        password_icon_data = Image.open("images/password-icon.png")
-
-        email_icon = CTkImage(dark_image=email_icon_data, light_image=email_icon_data, size=(20, 20))
-        password_icon = CTkImage(dark_image=password_icon_data, light_image=password_icon_data, size=(17, 17))
-
         self.frame = CTkFrame(master=self, width=556, height=745, fg_color="#ffffff")
         self.frame.pack_propagate(False)
         self.frame.pack(expand=True, side="right")
 
         CTkLabel(master=self.frame, text="Welcome Back!", text_color="#601E88", anchor="w", justify="left",
-                 font=("Arial Bold", 24)).pack(
-            anchor="w",
-            pady=(50, 5),
-            padx=(25, 0))
+                 font=("Arial Bold", 24)).pack(anchor="w", pady=(50, 5), padx=(25, 0))
 
         CTkLabel(master=self.frame, text="Sign in to your account", text_color="#7E7E7E", anchor="w", justify="left",
-                 font=("Arial Bold", 12)).pack(
-            anchor="w",
-            padx=(25, 0))
+                 font=("Arial Bold", 12)).pack(anchor="w", padx=(25, 0))
 
         CTkLabel(master=self.frame, text="  Email:", text_color="#601E88", anchor="w", justify="left",
-                 font=("Arial Bold", 14), image=email_icon, compound="left").pack(
-            anchor="w",
-            pady=(38, 0),
-            padx=(25, 0))
+                 font=("Arial Bold", 14), image=self.email_icon, compound="left").pack(anchor="w", pady=(38, 0), padx=(25, 0))
 
         self.username_entry = CTkEntry(master=self.frame, width=225, fg_color="#EEEEEE", border_color="#601E88",
-                                       border_width=1,
-                                       text_color="#000000")
-        self.username_entry.pack(
-            anchor="w",
-            padx=(25, 0))
+                                       border_width=1, text_color="#000000")
+        self.username_entry.pack(anchor="w", padx=(25, 0))
 
         CTkLabel(master=self.frame, text="  Password:", text_color="#601E88", anchor="w", justify="left",
-                 font=("Arial Bold", 14), image=password_icon, compound="left").pack(
-            anchor="w",
-            pady=(21, 0),
-            padx=(25, 0))
+                 font=("Arial Bold", 14), image=self.password_icon, compound="left").pack(anchor="w", pady=(21, 0), padx=(25, 0))
 
         self.password_entry = CTkEntry(master=self.frame, width=225, fg_color="#EEEEEE", border_color="#601E88",
-                                       border_width=1,
-                                       text_color="#000000", show="*")
-        self.password_entry.pack(
-            anchor="w",
-            padx=(25, 0))
+                                       border_width=1, text_color="#000000", show="*")
+        self.password_entry.pack(anchor="w", padx=(25, 0))
 
     def login(self):
         CTkButton(master=self.frame, text="Login", fg_color="#601E88", hover_color="#E44982", font=("Arial Bold", 12),
-                  text_color="#ffffff", width=225, command=self.perform_login).pack(
-            anchor="w",
-            pady=(40, 0),
-            padx=(25, 0))
+                  text_color="#ffffff", width=225, command=self.perform_login).pack(anchor="w", pady=(40, 0), padx=(25, 0))
 
     def perform_login(self):
         username = self.username_entry.get()
@@ -95,3 +67,12 @@ class LoginPage(CTkFrame):
 
         if self.user.login(self.database):
             self.app.after_logged_in(self.user)
+
+    def add_register(self):
+        CTkButton(master=self.frame, text="Register", fg_color="#601E88", hover_color="#E44982", font=("Arial Bold", 12),
+                  text_color="#ffffff", width=225, command=self.show_register_window).pack(anchor="w", pady=(15, 0), padx=(25, 0))
+
+    def show_register_window(self):
+        register_window = RegistrationWindow(self, self.database)
+        register_window.grab_set()
+
---
+++ gui/registration_window.py
@@ -0,0 +1,61 @@
+from tkinter import messagebox
+
+from PIL import Image
+from customtkinter import *
+
+from user import User
+
+
+class RegistrationWindow(CTkToplevel):
+    def __init__(self, parent, database):
+        super().__init__(parent)
+        self.title("Register")
+        self.geometry("500x300")
+
+        self.database = database
+        self.parent = parent
+
+        self.email_icon = CTkImage(dark_image=Image.open("images/email-icon.png"), size=(20, 20))
+        self.password_icon = CTkImage(dark_image=Image.open("images/password-icon.png"), size=(17, 17))
+
+        self.add_register_fields()
+        self.add_register_button()
+
+    def add_register_fields(self):
+        # Frame for email label and entry
+        register_frame = CTkFrame(master=self, fg_color="transparent")
+        register_frame.pack(anchor="center", pady=(50, 0))
+
+        CTkLabel(master=register_frame, text="Email:", text_color="#601E88", anchor="w", justify="left",
+                 font=("Arial Bold", 14), image=self.email_icon, compound="left").pack(
+            anchor="w", padx=(0, 0))
+
+        self.username_entry = CTkEntry(master=register_frame, width=225, fg_color="#EEEEEE",
+                                       border_color="#601E88",
+                                       border_width=1, text_color="#000000")
+        self.username_entry.pack(anchor="w", pady=(5, 0))
+
+        CTkLabel(master=register_frame, text="Password:", text_color="#601E88", anchor="w", justify="left",
+                 font=("Arial Bold", 14), image=self.password_icon, compound="left").pack(
+            anchor="w", padx=(0, 0))
+
+        self.password_entry = CTkEntry(master=register_frame, width=225, fg_color="#EEEEEE",
+                                       border_color="#601E88",
+                                       border_width=1, text_color="#000000", show="*")
+        self.password_entry.pack(anchor="w", pady=(5, 0))
+
+    def add_register_button(self):
+        CTkButton(master=self, text="Register", fg_color="#601E88", hover_color="#E44982",
+                  font=("Arial Bold", 12),
+                  text_color="#ffffff", width=225, command=self.perform_register).pack(anchor='center', pady=(15, 0))
+
+    def perform_register(self):
+        username = self.username_entry.get()
+        password = self.password_entry.get()
+        user = User(username, password)
+
+        if username and password:
+            user.register(self.database)
+            self.destroy()
+        else:
+            messagebox.showerror("Error", "Please fill in all fields")
