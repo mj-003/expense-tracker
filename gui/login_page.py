284287821e7e08d9d9afcdb82df824a70de9c2d0
mj-003 <272660@student.pwr.edu.pl>
2024-05-30 02:25:11 +0200

    gui log page

--- gui/login_page.py
+++ gui/login_page.py
@@ -30,41 +30,41 @@ class LoginPage(CTkFrame):
         self.add_register()
 
     def add_image(self):
-        side_img_data = Image.open("images/side-img.png")
-        side_img = CTkImage(dark_image=side_img_data, light_image=side_img_data, size=(400, 745))
+        side_img_data = Image.open("images/green_background3.jpeg")
+        side_img = CTkImage(dark_image=side_img_data, light_image=side_img_data, size=(430, 745))
 
         CTkLabel(master=self, text="", image=side_img).pack(expand=True, side="left")
 
     def add_log_pass(self):
-        self.frame = CTkFrame(master=self, width=556, height=745, fg_color="#ffffff")
+        self.frame = CTkFrame(master=self, width=556, height=745)
         self.frame.pack_propagate(False)
-        self.frame.pack(expand=True, side="right")
+        self.frame.pack(expand=True, fill="both", padx=(70,50))
 
-        CTkLabel(master=self.frame, text="Welcome Back!", text_color="#601E88", anchor="w", justify="left",
-                 font=("Arial Bold", 24)).pack(anchor="w", pady=(50, 5), padx=(25, 0))
+        CTkLabel(master=self.frame, text="Welcome Back!", text_color="#4b6053", anchor="w", justify="left",
+                 font=("Aptos", 35, 'bold')).pack(anchor="w", pady=(100, 5), padx=(25, 0))
 
-        CTkLabel(master=self.frame, text="Sign in to your account", text_color="#7E7E7E", anchor="w", justify="left",
-                 font=("Arial Bold", 12)).pack(anchor="w", padx=(25, 0))
+        CTkLabel(master=self.frame, text="Sign in to your account", text_color="#75975e", anchor="w", justify="left",
+                 font=("Arial Bold", 15)).pack(anchor="w", padx=(25, 0))
 
-        CTkLabel(master=self.frame, text="  Email:", text_color="#601E88", anchor="w", justify="left",
-                 font=("Arial Bold", 14), image=self.email_icon, compound="left").pack(anchor="w", pady=(38, 0),
+        CTkLabel(master=self.frame, text="  Username:", text_color="#658354", anchor="w", justify="left",
+                 font=("Aptos", 14), image=self.email_icon, compound="left").pack(anchor="w", pady=(38, 0),
                                                                                        padx=(25, 0))
 
-        self.username_entry = CTkEntry(master=self.frame, width=225, fg_color="#EEEEEE", border_color="#601E88",
-                                       border_width=1, text_color="#000000")
+        self.username_entry = CTkEntry(master=self.frame, width=255, fg_color="#EEEEEE", border_color="#4b6053",
+                                       border_width=1, text_color="#000000", placeholder_text='Your username')
         self.username_entry.pack(anchor="w", padx=(25, 0))
 
-        CTkLabel(master=self.frame, text="  Password:", text_color="#601E88", anchor="w", justify="left",
-                 font=("Arial Bold", 14), image=self.password_icon, compound="left").pack(anchor="w", pady=(21, 0),
+        CTkLabel(master=self.frame, text="  Password:", text_color="#658354", anchor="w", justify="left",
+                 font=("Aptos", 14), image=self.password_icon, compound="left").pack(anchor="w", pady=(21, 0),
                                                                                           padx=(25, 0))
 
-        self.password_entry = CTkEntry(master=self.frame, width=225, fg_color="#EEEEEE", border_color="#601E88",
+        self.password_entry = CTkEntry(master=self.frame, width=255, fg_color="#EEEEEE", border_color="#4b6053",
                                        border_width=1, text_color="#000000", show="*")
         self.password_entry.pack(anchor="w", padx=(25, 0))
 
     def login(self):
-        CTkButton(master=self.frame, text="Login", fg_color="#601E88", hover_color="#E44982", font=("Arial Bold", 12),
-                  text_color="#ffffff", width=225, command=self.perform_login).pack(anchor="w", pady=(40, 0),
+        CTkButton(master=self.frame, text="Login", fg_color="#658354", hover_color="#E44982", font=("Aptos", 12),
+                  text_color="#ffffff", width=255, command=self.perform_login).pack(anchor="w", pady=(40, 0),
                                                                                     padx=(25, 0))
 
     def perform_login(self):
@@ -77,9 +77,9 @@ class LoginPage(CTkFrame):
             self.app.after_logged_in(self.user, self.user_expenses)
 
     def add_register(self):
-        CTkButton(master=self.frame, text="Register", fg_color="#601E88", hover_color="#E44982",
-                  font=("Arial Bold", 12),
-                  text_color="#ffffff", width=225, command=self.show_register_window).pack(anchor="w", pady=(15, 0),
+        CTkButton(master=self.frame, text="Register", fg_color="#658354", hover_color="#E44982",
+                  font=("Aptos", 12),
+                  text_color="#ffffff", width=255, command=self.show_register_window).pack(anchor="w", pady=(15, 0),
                                                                                            padx=(25, 0))
 
     def show_register_window(self):
